8db78d606b6d323900ab5ee986d53741cc3d2adc
vijitha epa <vepa@bglcorp.com.au>
2019-07-13 14:43:47 +1000

    the use of React.createContext

---
+++ src/context-sample.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+function Parent({name}) {
+    return (
+        <div>
+            <h1>Parent</h1>
+            <Child name={name}/>
+        </div>
+    );
+}
+
+function Child({name}) {
+    return (
+        <div>
+            <h1>Child</h1>
+            <Grandchild name={name}/>
+        </div>
+    );
+}
+
+function Grandchild({name}) {
+    return (
+        <div>
+            <h1>Grandchild</h1>
+            <h3>Name: {name}</h3>
+        </div>
+    );
+}
+
+class App extends React.Component {
+    render() {
+        const name = 'Tyler';
+
+        return (
+            <Parent/>
+        );
+    }
+}
+
+render(<App/>, document.getElementById('root'));
