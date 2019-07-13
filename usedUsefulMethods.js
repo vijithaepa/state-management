fe4522af9164f8bd6431320bb0d60d25bbb010e9
vijitha epa <vepa@bglcorp.com.au>
2019-07-13 20:36:44 +1000

    Replaced the Provider and connect with ReactRedux library

--- usedUsefulMethods.js
+++ usedUsefulMethods.js
@@ -33,3 +33,54 @@ const checker = (store) => (next) => (action) => {
 function generateId() {
     return Math.random().toString(36).substring(2) + (new Date()).getTime().toString(36)
 }
+
+// Implementations of ReactRedux.Provider, ReactRedux.connect
+const Context = React.createContext();
+
+function connect(mapStateToProps) {
+    return (Component) => {
+
+        class Receiver extends React.Component {
+            componentDidMount() {
+                const {subscribe} = this.props.store
+
+                this.unsubscribe = subscribe(() => {
+                    this.forceUpdate()
+                })
+            }
+
+            componentWillUnmount() {
+                this.unsubscribe()
+            }
+
+            render() {
+                const {dispatch, getState} = this.props.store
+                const state = getState()
+                const stateNeeded = mapStateToProps(state)
+                return <Component {...stateNeeded} dispatch={dispatch}/>
+            }
+        }
+
+        class ConnectedComponent extends React.Component {
+            render() {
+                return (
+                    <Context.Consumer>
+                        {(store) => <Receiver store={store}/>}
+                    </Context.Consumer>
+                )
+            }
+        }
+        return ConnectedComponent
+    }
+}
+
+class Provider extends React.Component {
+
+    render() {
+        return (
+            <Context.Provider value={this.props.store}>
+                {this.props.children}
+            </Context.Provider>
+        )
+    }
+}
